13e1faf070370ef8ab5054f29728323e42d50049
jamesdgalbraith <galbraithenator@gmail.com>
2022-04-05 14:20:12 +0100

    Updated pipeline_outline, added suitable domains

--- pipeline_outline.sh
+++ pipeline_outline.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-# split repeatmodeler
-Rscript ~/projectDrive_2/DDE_Pipeline_2/splitter.R -f echisCarinatus-families.fa -p 256 -t DNA -o data/split
+export QUERY
 
-# list split files
-ls data/split/echisCarinatus-families.fa_seq_* | sed 's|data/split/||' > echisCarinatus-families.fa_split_seq_list.txt
+# Split fasta
+echo Splitting
+Rscript scripts/splitter.R -f seq/${QUERY} -p 2560 -t DNA -o data/split
 
-# make rps_out dir
-mkdir -p data/rps_out
+# List split files
+ls data/split/${QUERY}_seq_* | sed 's|data/split/||' > ${QUERY}_split_seq_list.txt
 
 # rpstblastn for known domains
-parallel --bar --jobs 128 -a split_seq_list.txt 'rpstblastn -query data/split/{} -db /media/projectDrive_1/databases/cdd/Cdd -out data/rps_out/{}.out -outfmt "6 qseqid qstart qend qlen sseqid sstart send slen pident length mismatch gapopen evalue bitscore qcovs stitle" -evalue 0.01 -num_threads 1'
-cat data/rps_out/echisCarinatus-families.fa_seq_* > data/echisCarinatus-families.fa.rps.out
+echo Blasting for domains
+mkdir -p data/rps_out
+parallel --bar --jobs 128 -a ${QUERY}_split_seq_list.txt 'rpstblastn -query data/split/{} -db /media/projectDrive_1/databases/cdd/Cdd -out data/rps_out/{}.out -outfmt "6 qseqid qstart qend qlen sseqid sstart send slen pident length mismatch gapopen evalue bitscore qcovs stitle" -evalue 0.01 -num_threads 1'
+cat data/rps_out/${QUERY}_seq_* > data/${QUERY}.rps.out
 
-# remove temp files
-rm data/rps_out/echisCarinatus-families.fa_seq_* data/split/echisCarinatus-families.fa_seq_*
+# dc-megablast against RepBase
+echo Blasting against RepBase
+mkdir -p data/repbase_out
+parallel --bar --jobs 128 -a ${QUERY}_split_seq_list.txt 'blastn -query data/split/{} -db /media/projectDrive_1/databases/repbase/RepBase5May2021.fasta -out data/repbase_out/{}.out -outfmt "6 qseqid qstart qend qlen sseqid sstart send slen pident length mismatch gapopen evalue bitscore qcovs" -task dc-megablast -num_threads 1'
+cat data/repbase_out/${QUERY}_seq_* > data/${QUERY}.repbase.out
 
-# run tandem repeat finder
-trf echisCarinatus-families.fa 2 7 7 80 10 50 500 -d -h
+# Run TRF
+mkdir -p data/trf_out
+parallel --bar --jobs 128 -a ${QUERY}_split_seq_list.txt 'trf data/split/{} 2 7 7 80 10 50 500 -d -h'
 
 # convert trf to gff
-python trf2gff.py -d echisCarinatus-families.fa.2.7.7.80.10.50.500.dat -o data/echisCarinatus-families.fa.trf.gff
-rm echisCarinatus-families.fa.2.7.7.80.10.50.500.dat
+parallel --bar --jobs 128 -a ${QUERY}_split_seq_list.txt 'python scripts/trf2gff.py -d {}.2.7.7.80.10.50.500.dat -o data/trf_out/{}.trf.gff'
+
+# compile trf
+cat data/trf_out/${QUERY}_seq_*.trf.gff > data/${QUERY}.trf.gff
+
+# remove temp files
+rm  data/split/${QUERY}_seq_* ${QUERY}*.2.7.7.80.10.50.500.dat data/trf_out/${QUERY}_seq_* ${QUERY}_split_seq_list.txt data/rps_out/${QUERY}_seq_*txt
+
+echo "all done"
